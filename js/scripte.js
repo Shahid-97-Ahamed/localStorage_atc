e1a792dc3b9908867b7380c5fd1c144d9e54da21
Shahid Ahamed <shahid.07ahamed@gmail.com>
2026-01-08 19:16:08 +0900

    first commit

---
+++ js/scripte.js
@@ -0,0 +1,9 @@
+/*how to value set in local storage*/
+
+const value = "10";
+
+localStorage.setItem("marks",value);
+localStorage.setItem("math",value);
+localStorage.setItem("science",value);
+localStorage.setItem("english",value);
+localStorage.setItem("history",value);
